72b36cb734beaac51532da6b0f98d777b6513efc
Anderson Acuña Obregon <Anderson Acua Obregon>
2016-09-22 06:45:20 -0500

    rehaciendo la primera version con creacion de elementos desde Javascript

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
 window.addEventListener("load", function() {
-    var agregarFormulario = document.getElementById("agregarForm");
-    var NuevoFormulario = document.getElementById("nuevoForm");
-    var addForm = document.getElementById("agregarForm");
+   var addForm = document.getElementById("agregarForm");
     
-    addForm.addEventListener("click", function() {
-        agregarFormulario.classList.add("d-none");
-        NuevoFormulario.classList.add("d-block");
+	addForm.addEventListener("click", function(e) {
+		event.preventDefault();
+		deleteElement();
+		addNewForm();
+		//deleteButtonAndAddText();
+		//addAggregator();
 	});
     
 });
+
+function deleteElement(){
+	var padre = document.getElementById("trello-body");
+	var elementToRemove = document.getElementById("agregarForm");
+	return padre.removeChild(elementToRemove);
+}
+
+function addNewForm(){
+
+	//agregando el form
+	var padre = document.getElementById("trello-body");
+	var form = document.createElement("form");
+	var newForm = padre.appendChild(form);
+	newForm.classList.add("formulario");
+	newForm.classList.add("b-radius");
+	//agregando input del form
+	var input = document.createElement("input");
+	var newInput = newForm.appendChild(input);
+	newInput.classList.add("entrada");
+	newInput.classList.add("b-radius");
+	//agregando boton 
+	var boton = document.createElement("button");
+	var newButton = newForm.appendChild(boton);
+	//newButton.textContent = ¨Añadir tarjeta¨;
+	newButton.classList.add("boton");
+	newButton.classList.add("btn");
+	newButton.classList.add("btn-primary");
+	
+}
+
+//newForm.setAttribute();
+
+
+
